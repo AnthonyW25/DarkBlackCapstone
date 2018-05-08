d86921de740e057288b03fa7af7f6bd57780f812
MgodbyYo <mikegodby703@gmail.com>
2018-05-08 14:49:32 -0700

    add heroku creds and update default rout to hit the DB

--- config/database.php
+++ config/database.php
@@ -1,5 +1,10 @@
 <?php
 
+$url = parse_url(getenv("DATABASE_URL"));
+$host = $url["host"]??null;
+$username = $url["user"]??null;
+$password = $url["pass"]??null;
+$database = substr($url["path"], 1)??null;
 return [
 
     /*
@@ -41,11 +46,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $host, 					//env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => $database, 			//env('DB_DATABASE', 'forge'),
+            'username' => $username,            //env('DB_USERNAME', 'forge'),
+            'password' => $password,			//env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
