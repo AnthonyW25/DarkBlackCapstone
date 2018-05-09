5754608533dc8f3daf6adb793e39cee428aa84f7
MgodbyYo <mikegodby703@gmail.com>
2018-05-09 14:22:12 -0700

    authentication has been set

--- config/database.php
+++ config/database.php
@@ -1,10 +1,5 @@
 <?php
 
-$url = parse_url(getenv("DATABASE_URL"));
-$host = $url["host"]??null;
-$username = $url["user"]??null;
-$password = $url["pass"]??null;
-$database = substr($url["path"], 1)??null;
 return [
 
     /*
@@ -46,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => $host, 					//env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => $database, 			//env('DB_DATABASE', 'forge'),
-            'username' => $username,            //env('DB_USERNAME', 'forge'),
-            'password' => $password,			//env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
