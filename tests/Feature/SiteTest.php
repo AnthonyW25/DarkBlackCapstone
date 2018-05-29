9670513932cb2e1f13e6186397ff880b13ea1d6f
Dirk <dirkbritton@gmail.com>
2018-05-29 11:36:48 -0700

    Site Test

---
+++ tests/Feature/SiteTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Sale;
+use App\Site;
+use Carbon\Carbon;
+use Tests\TestCase;
+
+class SiteTest extends TestCase
+{
+    /** @test */
+    public function provides_sales_at_date()
+    {
+        $site = new Site();
+
+        $five_days_ago = Carbon::now()->subDay(5)->toDateString();
+
+        $sales = $site->salesOn($five_days_ago);
+
+        $this->assertInstanceOf(Sale::class, $sales);
+
+        $this->assertEquals($five_days_ago, $sales->date);
+    }
+}
