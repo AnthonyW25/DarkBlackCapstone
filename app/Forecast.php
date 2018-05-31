a32c37257c3b2651c80b5af3e33f0d921b6fda5a
MgodbyYo <mikegodby703@gmail.com>
2018-05-30 19:01:22 -0700

    working on sales forecast. can set up growth rate now

--- app/Forecast.php
+++ app/Forecast.php
@@ -7,6 +7,7 @@
  */
 
 namespace App;
+use DB;
 
 class Forecast
 {
@@ -18,6 +19,29 @@ class Forecast
     public function __construct(Site $site, $date = null)
     {
         $this->site = $site;
+    }
+
+    public function forecastCalculation(int $percent){
+			DB::table('sales')
+    			->where('site_id', '=', $this->site->id)
+    			->update(['forecast_percentage' => $percent]);
+
+    		$sales = Sale::where('site_id', '=', $this->site->id)
+    			->orderBy('id', 'desc')
+    			->first();
+
+    		$seven_day_avg = $sales->seven_day_average;
+
+    		$this->seven_day = ($seven_day_avg + ($seven_day_avg * ($percent/100)));
+
+    }
+
+    public function getPercentage(){
+    	$sales = Sale::where('site_id', '=', $this->site->id)
+    			->orderBy('id', 'desc')
+    			->first();
+
+    	$this->growth_rate = $sales->forecast_percentage;
     }
 
 
